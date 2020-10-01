b376109ed0b0f6406923858b6b4a6a4491d17544
Emeka <emekannecam@gmail.com>
2020-09-30 20:17:51 -0500

    quizpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:quizzler/questionbank.dart';
 
 void main() {
   runApp(QuizApp());
@@ -28,19 +28,7 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scores = [];
 
-  List<String> questions = [
-    'Q1.Software is a product and can be manufactured using the same technologies used for other engineering artifacts',
-    'Q2.Software engineering umbrella activities are only applied during the initial phases of software development projects.',
-    'Q3. ahead for software reuse reduces the cost and increases the value of the systems into which they are incorporated.',
-    'Q4.The essence of software engineering practice might be described as understand the problem, plan a solution, carry out the plan, and examine the result for accuracy.',
-    'Q5.In agile process models the only deliverable work product is the working program.',
-    'Q6.A most software development projects are initiated to try to meet some business need.',
-    'Q7.In general software only succeeds if its behavior is consistent with the objectives of its designers.',
-  ];
-
-  List<bool> answers = [false, false, true, true, false, true, false];
-
-  int qnum = 0;
+  QuestionBank questionBank = QuestionBank();
 
   @override
   Widget build(BuildContext context) {
@@ -50,11 +38,15 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           flex: 4,
           child: Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 0, 20.0, 0),
+            padding: const EdgeInsets.fromLTRB(30.0, 0, 30.0, 0),
             child: Center(
-              child: Text(
-                questions[qnum],
-                style: TextStyle(fontSize: 20, color: Colors.black),
+              child: Card(
+                margin: EdgeInsets.symmetric(),
+                clipBehavior: Clip.hardEdge,
+                child: Text(
+                  questionBank.getQuestions(),
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
               ),
             ),
           ),
@@ -65,20 +57,20 @@ class _QuizPageState extends State<QuizPage> {
             child: FlatButton(
               color: Colors.blue,
               onPressed: () {
-                bool correctAnswer = answers[qnum];
+                bool correctAnswer = questionBank.getAnswer();
                 if (correctAnswer == true) {
                   scores.add(Icon(
-                    Icons.check,
+                    Icons.exposure_plus_1,
                     color: Colors.blue,
                   ));
                 } else
                   scores.add(Icon(
-                    Icons.clear,
+                    Icons.exposure_zero,
                     color: Colors.red,
                   ));
 
                 setState(() {
-                  qnum++;
+                  questionBank.nextQuestion();
                 });
               },
               child: Text(
@@ -94,21 +86,21 @@ class _QuizPageState extends State<QuizPage> {
             child: FlatButton(
               color: Colors.red,
               onPressed: () {
-                bool correctAnswer = answers[qnum];
+                bool correctAnswer = questionBank.getAnswer();
 
                 if (correctAnswer == false) {
                   scores.add(Icon(
-                    Icons.check,
+                    Icons.exposure_plus_1,
                     color: Colors.blue,
                   ));
                 } else
                   scores.add(Icon(
-                    Icons.clear,
+                    Icons.exposure_zero,
                     color: Colors.red,
                   ));
 
                 setState(() {
-                  qnum++;
+                  questionBank.nextQuestion();
                 });
               },
               child: Text(
---
+++ lib/question_answer.dart
@@ -0,0 +1,9 @@
+class QuestionAnswer {
+  String question;
+  bool answer;
+
+  QuestionAnswer({String q, bool a}) {
+    question = q;
+    answer = a;
+  }
+}
